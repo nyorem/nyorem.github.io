c560de09d97cffa3815a0b3292dfc3063d270ac6
Jocelyn MEYRON <jmeyron@gmail.com>
2021-06-07 18:30:36 +0200

    deploy

---
+++ _next/static/uQgLg36RckTeTRL-xH9Ym/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftranslation\u002F[lang]\u002F[slug]\u002F[chapter]","\u002Ftranslation\u002F[lang]\u002F[slug]","\u002Fpage\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
